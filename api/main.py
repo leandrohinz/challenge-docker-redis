2b9d4aa5b5cf32ac769bda9c43429664f686a0a5
Leandro Hinestroza <leandro.hinestroza@perficient.com>
2024-02-26 16:39:17 -0500

    changes

--- api/main.py
+++ api/main.py
@@ -1,28 +1,61 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 import redis
 
 app = Flask(__name__)
 redis_db = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
+
+
+app = Flask(__name__)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+# Configurar las credenciales para la autenticación básica
+app.config['BASIC_AUTH_USERNAME'] = 'username'
+app.config['BASIC_AUTH_PASSWORD'] = 'password'
+
+# Inicializar la extensión de autenticación básica
+basic_auth = BasicAuth(app)
+
+# Función de autenticación para verificar las credenciales
+@basic_auth.verify_password
+def verify_password(username, password):
+    # Verificar las credenciales del usuario
+    if username == app.config['BASIC_AUTH_USERNAME'] and password == app.config['BASIC_AUTH_PASSWORD']:
+        return True
+    return False
+
 @app.route('/api/queue/pop', methods=['POST'])
-def popmessage():
-    message = redis_db.lpop('message_queue')
+@basic_auth.required
+def pop_message():
+    message = redis_client.lpop('message_queue')
     if message:
         return jsonify({'status': 'ok', 'message': message.decode('utf-8')}), 200
     else:
         return jsonify({'status': 'ok', 'message': None}), 200
 
 @app.route('/api/queue/push', methods=['POST'])
+@basic_auth.required
 def push_message():
     message = request.data.decode('utf-8')
-    redis_db.rpush('message_queue', message)
+    redis_client.rpush('message_queue', message)
     return jsonify({'status': 'ok'}), 200
 
 @app.route('/api/queue/count', methods=['GET'])
+@basic_auth.required
 def get_queue_count():
-    count = redis_db.llen('message_queue')
+    count = redis_client.llen('message_queue')
     return jsonify({'status': 'ok', 'count': count}), 200
 
+# Endpoint to get data from Redis
+@app.route('/get/<key>', methods=['GET'])
+def get_data(key):
+    value = redis_db.get(key)
+    if value:
+        return jsonify({key: value.decode('utf-8')})
+    else:
+        return jsonify({'error': 'Key not found!'}), 404
+    
 # Endpoint to add data in Redis
 @app.route('/add', methods=['POST'])
 def add_data():
@@ -35,6 +68,9 @@ def add_data():
                       'status': 'OK!' })
     else:
         return jsonify({'error': 'Invalid key or value provided!'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # Endpoint to POP data in Redis
 # @app.route('/api/queue/pop', methods=['POST'])
@@ -74,20 +110,6 @@ def add_data():
 #                       'status': 'OK!' })
 #     else:
 #         return jsonify({'error': 'Invalid key or value provided!'}), 200   
-
-# Endpoint to get data from Redis
-@app.route('/get/<key>', methods=['GET'])
-def get_data(key):
-    value = redis_db.get(key)
-    if value:
-        return jsonify({key: value.decode('utf-8')})
-    else:
-        return jsonify({'error': 'Key not found!'}), 404
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 
 
