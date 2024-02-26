b567368ff06ef06c0752d3fa85df27c8c40b1526
Leandro Hinestroza <leandro.hinestroza@perficient.com>
2024-02-26 15:12:46 -0500

    files added

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 import redis
 
 app = Flask(__name__)
-redis_db = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_db = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
 
 # Sample endpoint to set data in Redis
 @app.route('/set', methods=['POST'])
@@ -15,6 +15,31 @@ def set_data():
         return jsonify({'message': 'Data set successfully!'})
     else:
         return jsonify({'error': 'Invalid key or value provided!'}), 400
+    
+# Sample endpoint to set data in Redis
+@app.route('/add', methods=['POST'])
+def add_data():
+    data = request.get_json()
+    key = data.get('key')
+    value = data.get('value')
+    if key and value:
+        redis_db.set(key, value)
+        return jsonify({'message': 'Data added successfully!'})
+    else:
+        return jsonify({'error': 'Invalid key or value provided!'}), 400
+
+# Sample endpoint to set data in Redis
+@app.route('/pop', methods=['POST'])
+def pop_data():
+    data = request.get_json()
+    key = data.get('key')
+    value = data.get('value')
+    if key and value:
+        redis_db.set(key, value)
+        return jsonify({'message': 'Data added successfully!',
+                      'status': 'OK!' })
+    else:
+        return jsonify({'error': 'Invalid key or value provided!'}), 200
 
 # Sample endpoint to get data from Redis
 @app.route('/get/<key>', methods=['GET'])
@@ -24,6 +49,11 @@ def get_data(key):
         return jsonify({key: value.decode('utf-8')})
     else:
         return jsonify({'error': 'Key not found!'}), 404
+    
+# Sample endpoint to set data in Redis
+@app.route('/prueba', methods=['GET'])
+def prueba():
+    return jsonify({'message': 'Data get successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
